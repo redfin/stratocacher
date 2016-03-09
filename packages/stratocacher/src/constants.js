799b8226579e25927b532569e9a33893629b7587
Bo Borgerson <gigabo@gmail.com>
2016-03-09 15:57:33 -0800

    Default TTL is forever

--- packages/stratocacher/src/constants.js
+++ packages/stratocacher/src/constants.js
@@ -1,5 +1,5 @@
 export const GLOBAL_VERSION    = 0;
-export const DEFAULT_TTL       = ONE_HOUR;
+export const DEFAULT_TTL       = 0;
 export const DEFAULT_TTR_RATIO = 0.8;
 export const ONE_SECOND        = 1000;
 export const ONE_MINUTE        = 60 * ONE_SECOND;
