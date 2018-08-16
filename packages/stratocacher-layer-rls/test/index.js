c6c72a1b7a6316991aa5a338247e7b5eed0c4ebf
Matt Olson <matt.olson@redfin.com>
2018-08-16 10:53:57 -0700

    Use request-local-storage in LayerRLS test

--- packages/stratocacher-layer-rls/test/index.js
+++ packages/stratocacher-layer-rls/test/index.js
@@ -1,24 +1,16 @@
-const mockRequire = require("mock-require");
-
-var CACHE = {};
-
-mockRequire("request-local-storage", {
-	getNamespace: () => CACHE,
-});
-
+const RequestLocalStorage = require('request-local-storage');
 const LayerRLS = require("../lib/index.js").default;
 
 describe("A LayerRLS instance", () => {
 	const obj = { foo: "bar" }
 
 	it("sets and gets a value", done => {
-
-		const layer = new LayerRLS({key: "A"});
-
-		layer.set(obj).then(() => layer.get()).then(() => {
-			expect(layer.val).toEqual(obj);
-			done();
+		RequestLocalStorage.startRequest(() => {
+			const layer = new LayerRLS({key: "A"});
+			layer.set(obj).then(() => layer.get()).then(() => {
+				expect(layer.val).toEqual(obj);
+				done();
+			});
 		});
 	});
-
 });
