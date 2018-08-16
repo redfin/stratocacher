4fa47512a7a28af039e639dfbf4023ce1b4b4f02
Matt Olson <matt.olson@redfin.com>
2018-08-16 10:53:33 -0700

    Request one RLS namespace at the module level

--- packages/stratocacher-layer-rls/src/index.js
+++ packages/stratocacher-layer-rls/src/index.js
@@ -2,14 +2,7 @@ import {getNamespace} from "request-local-storage";
 import {Layer} from "stratocacher";
 import Q from "q";
 
-function getCache() {
-	const rls = getNamespace();
-	if (!rls.stratocacher) {
-		rls.stratocacher = {};
-	}
-	return rls.stratocacher;
-}
-
+const RLS = getNamespace();
 /*
 	Layer that uses request-local-storage as the cache backing.
 	See: https://www.npmjs.com/package/request-local-storage
@@ -17,14 +10,14 @@ function getCache() {
 export default class LayerRLS extends Layer {
 
 	get() {
-		const val = getCache()[this.key];
+		const val = RLS()[this.key];
 		this.load(val);
 		return Q();
 	}
 
 	set(val) {
 		val = this.dump(val);
-		getCache()[this.key] = val;
+		RLS()[this.key] = val;
 		return Q();
 	}
 }
